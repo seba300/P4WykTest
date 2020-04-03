f97ace3c4f72033ff75480460b6aaba52fc79126
seba300 <sebastiandomagala98@gmail.com>
2020-04-03 15:02:20 +0200

    first commit

---
+++ EFCinema/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace EFCinema
+{
+    [Table("Cinema")]
+    public class CinemaModel
+    {
+        [Key]
+        public int CinemaId { get; set; }
+        public string Name { get; set; }
+        public string CinemaAddress { get; set; }
+
+        public virtual RoomModel Room { get; set; }
+        public virtual AddressModel Address { get; set; }
+    }
+
+    [Table("Room")]
+    public class RoomModel
+    { 
+        [Key]
+        public int RoomId { get; set; }
+        public string Name { get; set; }
+        public int SeatNumbers { get; set; }
+
+        public virtual ICollection<CinemaModel> Cinema { get; set; }
+        public virtual PlaceModel Place { get; set; }
+    }
+
+    [Table("Place")]
+    public class PlaceModel
+    { 
+        [Key]
+        public int PlaceId { get; set; }
+        public string Row { get; set; }
+        public int Number { get; set; }
+
+        public virtual ICollection<RoomModel> Room { get; set; }
+        public virtual TicketModel Ticket { get; set; }
+
+    }
+
+    [Table("Seance")]
+    public class SeanceModel
+    {
+        [Key]
+        public int SeanceId { get; set; }
+        public string MovieName { get; set; }
+        public DateTime MovieStart { get; set; }
+
+       public virtual TicketModel Ticket { get; set; }
+       
+    }
+
+    [Table("Ticket")]
+    public class TicketModel
+    {
+        [Key]
+        public int TicketId { get; set; }
+        public decimal Price { get; set; }
+
+        public virtual ICollection<PlaceModel> Place { get; set; }
+        public virtual ICollection<SeanceModel> Seance { get; set; }
+    }
+
+    [Table("Address")]
+    public class AddressModel
+    {
+        [Key]
+        [ForeignKey("CinemaModel")]
+        public int CinemaId { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string HomeNumber { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public virtual CinemaModel Cinema { get; set; }
+    }
+
+    public class CinemaContext : DbContext
+    {
+        public CinemaContext() : base("CinemaContext") { }
+        public virtual DbSet<CinemaModel> Cinemas { get; set; }
+        public virtual DbSet<RoomModel> Rooms { get; set; }
+        public virtual DbSet<PlaceModel> Places { get; set; }
+        public virtual DbSet<SeanceModel> Seances { get; set; }
+        public virtual DbSet<TicketModel> Tickets { get; set; }
+        public virtual DbSet<AddressModel> Addresses { get; set; }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
